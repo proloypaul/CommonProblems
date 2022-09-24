673c3992fa227c57d8d1d86fd79a631a15990a6e
proloypaul <proloyp23@gmail.com>
2022-09-24 22:02:21 +0600

    grade and largest

--- CommonProblem/firstFile.js
+++ CommonProblem/firstFile.js
@@ -12,7 +12,7 @@ const helpedSarah = () => {
     return remainderValue;
 }
 
-console.log("Remainder value would be " + helpedSarah());
+// console.log("Remainder value would be " + helpedSarah());
 
 // Question 
 /* 
@@ -29,7 +29,7 @@ const harryProblem = () => {
     const returnMoney = gavenTk - totalOrangeAndApple;
     return returnMoney;
 }
-console.log(harryProblem());
+// console.log(harryProblem());
 // Question
 /* 
 John’s teacher gave him two variables. Each variable contains a string.
@@ -38,8 +38,54 @@ be’ and ‘an awesome web developer’) and print them in one line. Help
 John write the program.
 */
 // solved
-
 const firstVar = "I am going to be";
 const secondVar = "an awesome web developer";
 const combineTwoVar = `'${firstVar}' and '${secondVar}'`; 
-console.log(combineTwoVar);
+// console.log(combineTwoVar);
+
+// Question
+/* 
+You and your friends Tom, Jane, Peter and John got their final exam
+results. Your total score is 85, Tom’s total score is 66, Jane’s total score is
+95, Peter’s total score is 56 and John’s total score is 40. The grading
+chart is
+*/
+const checkGrade = (name, grade) => {
+    if(grade >= 80){
+        console.log(`${name} grade A`);
+    }else if(grade >= 60){
+        console.log(`${name} grade B`);
+    }else if(grade >= 50){
+        console.log(`${name} grade C`);
+    }else if(grade >= 40){
+        console.log(`${name} grade D`);
+    }else{
+        console.log(`${name} grade F`);
+    }
+}
+
+// checkGrade("Shohag", 86);
+// checkGrade("Tom's", 66);
+// checkGrade("Jane's", 95);
+// checkGrade("Peter's", 56);
+// checkGrade("John's", 40);
+
+// Question
+/* 
+You are given three numbers 13, 79, and 45. Write a program that will
+print the largest number using if-else.
+*/
+
+// solved
+const firstNum = 13;
+const secondNum = 79;
+const thirdNum = 45;
+if(secondNum<firstNum && firstNum>thirdNum){
+    console.log("largest Number: " + firstNum);
+}else if(secondNum>firstNum && secondNum>thirdNum){
+    console.log("largest Number: " + secondNum);
+}else{
+    console.log("largest Number: " + thirdNum);
+}
+
+

76263c1295a36efb32284b1894f4e6da7c842736
proloypaul <proloyp23@gmail.com>
2023-01-15 01:35:08 +0600

    solved student data

---
+++ ProblemWithC/Concatinate.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    // int a;
+    char b[100];
+    // scanf("%d \n", &a);
+    scanf("%s", &b);
+    printf("display a value b=%s", b);
+
+    return 0;
+}
+
+
+
